ccaa85669bee8ac672dc2e46ce826d875b5ed687
cf4As <cfoxgrover@aaaa.org>
2017-02-26 23:28:53 -0500

    2nd commit

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,19 +4,5 @@
 <?php snippet('header') ?>
 
 
-<div class="container">
-	<div class="col-lg-4">
-		<?php echo $site->title() ?>
-  	</div>
-
-  	<div class="col-lg-4">
- 	<?php echo $site->text() ?>
-	</div>
-
-  	<div class="col-lg-4">
- 	<?php echo $site->body() ?>
-	</div>
-</div>
-
 
 <?php snippet('footer') ?>
--- site/templates/home.php
+++ site/templates/home.php
@@ -3,6 +3,17 @@
 
 <?php snippet('header') ?>
 
-  
+<main>
+<div class="container home-header">
+	<div class= "row">
+  		<div class="col-lg-6">
+ 			<h1><?= $page->headertext() ?></h1>
+		</div>
+  		<div class="col-lg-6">
+ 			<?php echo $page->body() ?>
+		</div>
+	</div>
+</div>
+</main>
 
 <?php snippet('footer') ?>
