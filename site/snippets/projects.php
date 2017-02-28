2d08f421dd069aa87e2fa91c5bbcb9ecca633224
cf4As <cfoxgrover@aaaa.org>
2017-02-27 23:08:52 -0500

    at 13:48 in video 2

---
+++ site/snippets/projects.php
@@ -0,0 +1,27 @@
+<div class="row projects">
+	<div class="col-lg-4">
+		<div class="project">
+			<div class="project-image">
+				<img src="http://placehold.it/300x300">
+			</div>
+			<div class="project-description">
+
+			</div>
+		</div>
+	</div>
+	
+	<div class="col-lg-4">
+		<div class="project-image">
+
+		</div>
+	</div>
+
+	<div class="col-lg-4">
+		<div class="project-image">
+
+		</div>
+	</div>
+
+</div>
+
+
--- site/templates/home.php
+++ site/templates/home.php
@@ -4,16 +4,24 @@
 <?php snippet('header') ?>
 
 <main>
-<div class="container home-header">
-	<div class= "row">
-  		<div class="col-lg-6">
- 			<h1><?= $page->headertext() ?></h1>
-		</div>
-  		<div class="col-lg-6">
- 			<?php echo $page->body() ?>
+	<div class="container home-header">
+		<div class= "row">
+  			<div class="col-lg-6">
+ 				<h1><?= $page->headertext() ?></h1>
+			</div>
+  			<div class="col-lg-6">
+ 				<?php echo $page->body() ?>
+			</div>
 		</div>
 	</div>
-</div>
+	<div class="container">
+		<div class="divider-small">
+			<a href="#">Title Page</a>
+			<hr>
+		</div>
+	<div class="container">
+		<?php snippet('projects'); ?>
+	</div>	
 </main>
 
 <?php snippet('footer') ?>
